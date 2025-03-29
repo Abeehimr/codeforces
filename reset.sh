f28fdf8b5d78e6d6c733eaa419fc6e0fbef3f80b
Abeehimr <ibraheemkureshi@gmail.com>
2025-03-29 19:11:31 +0500

    shell commands

---
+++ reset.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+cp tem.py A.py
+cp tem.py B.py
+cp tem.py C.py
+cp tem.py D.py
+cp tem.py E.py
+cp tem.py F.py
---
+++ stress.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+while true; do
+    python $1 > output.txt
+
+    brute=$(python $2 < input.txt)
+    actual=$(python $3 < input.txt)
+
+    if [ "$brute" != "$actual" ]; then
+        echo "Outputs not match:"
+        echo "Brute force output:"
+        echo "$brute"
+        echo "Actual output:"
+        echo "$actual"
+        break
+    fi
+done
