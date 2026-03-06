71cb4280a725a411285622c90f900bed7d166547
Abeehimr <ibraheemkureshi@gmail.com>
2026-03-06 14:01:46 +0500

    done

--- stress.sh
+++ stress.sh
@@ -1,13 +1,9 @@
 #! /bin/bash
-
 while true; do
     python t.py > input.txt
-
     brute=$(python $1 < input.txt)
     actual=$(python $2 < input.txt)
-
     if [ "$brute" != "$actual" ]; then
-        echo "Outputs not match:"
         echo "Brute force output:"
         echo "$brute"
         echo "Actual output:"
